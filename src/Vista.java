ad579af5e0c12a701cc744a3e8c8de7c1375929f
EstebanZG999 <zam22119@uvg.edu.gt>
2023-02-20 18:47:16 -0600

    Initial commit

---
+++ src/Vista.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Vista {
+
+    private Scanner scan;
+
+    public Vista(){
+        scan = new Scanner(System.in);
+    }
+
+    public String menu(){
+        System.out.println("A");
+        return null;
+    }
+}
