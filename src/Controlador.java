c080b3b40ad8d99a925a4dd92daed2d3c1645e0f
EstebanZG999 <zam22119@uvg.edu.gt>
2023-02-21 18:54:48 -0600

    2

--- src/Controlador.java
+++ src/Controlador.java
@@ -5,10 +5,21 @@ public class Controlador {
     Scanner scan = new Scanner(System.in);
 
     String eleccion;
+    IStack<String> stack;
+    IList<String> list;
 
     public void principal(){
         System.out.println("Escribir la eleccion que desea realizar: Vector, Arraylist, Lists");
         eleccion = scan.next();
-        eleccion.toLowerCase();
+        eleccion = eleccion.toLowerCase();
+        if(eleccion.equals("Vector")){
+            IStack<String> stack = (new StackFactory<String>().getInstance(eleccion));
+        }
+        if(eleccion.equals("Arraylist")){
+            IStack<String> stack = (new StackFactory<String>().getInstance(eleccion));
+        }
+        if(eleccion.equals("Lists")){
+            IList<String> list = (new ListFactory<String>().getInstance(eleccion));
+        }
     }
 }
